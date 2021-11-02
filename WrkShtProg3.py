24125ff646aa2f97f780deffd8eda291b1cc61fd
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2021-11-02 19:10:30 +0000

    Update WrkShtProg3.py

--- WrkShtProg3.py
+++ WrkShtProg3.py
@@ -1,5 +1,4 @@
 import random
-import math
 a=int(input("Give a number:    "))
 b=int(input("Give a lower number:    "))
 random_choice = random.randint(a, b)
@@ -9,11 +8,13 @@ if a>b:
 else:
     upper=b
     lower=a
-min_number_guesses = round(math.log(upper - lower + 1, 2))
-number_of_guesses = 0
-while (number_of_guesses <= min_number_guesses):
+max_number_guesses =(upper - lower + 1)
+print ("You have a max of ", max_number_guesses, "guesses")
+while (0 < max_number_guesses):
     guess=int(input("Guess a number between them:   "))
+    number_of_guesses = max_number_guesses - 1
     if guess == random_choice:
-        print("Correct. That took you")
+        print("Correct. That took you", max_number_guesses - number_of_guesses, "guesses")
     else:
         print("You missed that one, try another!")
+        max_number_guesses-1
