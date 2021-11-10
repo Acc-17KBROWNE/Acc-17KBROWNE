d1f0d23d8b1ed6b694ea8ea928c197573bea8172
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2021-11-10 15:14:00 +0000

    Add files via upload

---
+++ PthWrkSht6.py
@@ -0,0 +1,43 @@
+import time
+'''string="Hello There Bob"
+string_length=len(string)
+print(string_length)
+print(string[0])
+print(string[4])
+print(string[-1])
+time.sleep(2)
+for ch in string:
+    print(ch)
+time.sleep(2)
+for ch in range(0,len(string)):
+    print(string[ch])
+time.sleep(2)
+for ch in range(0,len(string)):
+    print(ch)
+
+time.sleep(1)
+'''
+string0="Can I get a bread with no gluten?"
+print(string0)
+time.sleep(1)
+string.upper()
+time.sleep(1)
+string.lower()
+time.sleep(1)
+string.count(x)
+time.sleep(1)
+string.find(x)
+time.sleep(1)
+string.replace(x,y)
+time.sleep(1)
+string.islower()
+time.sleep(1)
+string.isupper()
+time.sleep(1)
+string.isalnum()
+time.sleep(1)
+string.isalpha()
+time.sleep(1)
+string.isdigit()
+string.index(s)
+string.strip(x)
