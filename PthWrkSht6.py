6a502aa2e091bdf0a319550315528bee9cbcbf33
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2021-11-10 19:08:26 +0000

    Update PthWrkSht6.py

--- PthWrkSht6.py
+++ PthWrkSht6.py
@@ -1,5 +1,5 @@
 import time
-'''string="Hello There Bob"
+string="Hello There Bob"
 string_length=len(string)
 print(string_length)
 print(string[0])
@@ -16,28 +16,40 @@ for ch in range(0,len(string)):
     print(ch)
 
 time.sleep(1)
-'''
-string0="Can I get a bread with no gluten?"
+
+string0=" Can I get a bread with no gluten? "
 print(string0)
 time.sleep(1)
-string.upper()
+print(string0.upper())
 time.sleep(1)
-string.lower()
+print(string0.lower())
 time.sleep(1)
-string.count(x)
+print(string0.count("a"))
 time.sleep(1)
-string.find(x)
+print(string0.find("a"))
 time.sleep(1)
-string.replace(x,y)
+replaced = string0.replace("a", "o")
+print(replaced)
 time.sleep(1)
-string.islower()
+print(string0.islower())
 time.sleep(1)
-string.isupper()
+print(string0.isupper())
 time.sleep(1)
-string.isalnum()
+print(string0.isalnum())
 time.sleep(1)
-string.isalpha()
+print(string0.isalpha())
 time.sleep(1)
-string.isdigit()
-string.index(s)
-string.strip(x)
+print(string0.isdigit())
+time.sleep(1)
+result=string0.index("no")
+print(result)
+time.sleep(1)
+print(string0.strip(" "))
+
+time.sleep(1)
+pal=str(input("Give me a word: "))
+pal=pal.upper()
+if(pal==pal[::-1]):
+    print("The word is a palindrome")
+else:
+    print("Not a palindrome")
