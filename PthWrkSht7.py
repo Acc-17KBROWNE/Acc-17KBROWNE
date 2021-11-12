6b061623d5450233ea505d11fe2043f4b55719bb
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2021-11-12 15:10:15 +0000

    Add files via upload

---
+++ PthWrkSht7.py
@@ -0,0 +1,18 @@
+import time
+print("""Python
+programming
+is
+great
+fun! :)
+""")
+
+time.sleep(1)
+string=str(input("Give me a nice keyboard smash: "))
+finalnum=0
+for i in (string):
+    if i.isdigit():
+        finalnum+=int(i)
+print(finalnum)
+
+sentence=str(input("Write a sentence or i take yor liver: "))
+print(sentence.replace(" ","-"))
