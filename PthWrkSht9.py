ce39930e7a841c08dce2f759fcbfb28df3d4906d
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2021-12-02 21:16:02 +0000

    Update PthWrkSht9.py

--- PthWrkSht9.py
+++ PthWrkSht9.py
@@ -1,3 +1,4 @@
+import statistics
 file1=open("C:/Users/17KBROWNE.acc/Downloads/10Random.txt", "r")
 for line in file1:
     print(line)
@@ -16,26 +17,30 @@ for i in range(len(list1)):
     freq1+=1
     if list1[i] == list1[i-1]:
         freq1+=1
-    print(list1[i], "appears", freq1, "times")
     freq1list.append(freq1)
     freq1=0
+for i in range(len(freq1list)):
+    if freq1list[i] == max(freq1list):
+        print(list1[i], "is the mode")
+print(statistics.median(list1), "is the median")
 
 file2=open("C:/Users/17KBROWNE.acc/Downloads/100Random.txt", "r")
+list2=[]
 for line in file2:
     print(line)
-    listo=line
-list2=[]
-for item in listo.split():
-    list2.append(int(item))
+    for item in line.split():
+        list2.append(int(item))
 list2==list2.sort()
 range2=(list2[-1]-list2[0])
 print("Range is", range1)
 mean2=sum(list2)/10
 print("Mean is", mean2)
-freq1=0
-for i in range(len(list1)):
-    freq1+=1
-    if list1[i] == list1[i-1]:
-        freq1+=1
-    print(list1[i], "appears", freq1, "times")
-    freq1=0
+freq2=0
+freq2list={}
+for i in list2:
+    if i in freq2list:
+        freq2list[i] +=1
+    else:
+        freq2list[i] =1
+print(max(freq2list))
+print(statistics.median(list1), "is the median")
