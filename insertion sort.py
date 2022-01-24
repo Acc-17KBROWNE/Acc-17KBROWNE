62fb3ba9ec4b5076a996666137f16dac3cc8d926
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2022-01-24 09:17:23 +0000

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,60 @@
+import random
+list1=[]
+choice=int(input("How many would you like to add to the list? "))
+for i in range(choice):
+    number=int(input("Add to list: "))
+    list1.append(number)
+random.shuffle(list1)
+def bubblesort(list1, descending=False, dbg=False, insert=False):
+    print("Starting list: ", list1)
+    exchange = True
+    n = len(list1)
+    i = 0
+    marker=1
+    temp=0
+    while (i< n) and exchange:
+        exchange = False
+        for j in range(n-i-1):
+            if descending==False:
+                if insert==False:
+                    if list1[j] > list1[j+1]:
+                        num=list1[j]
+                        list1[j]=list1[j+1]
+                        list1[j+1]=num
+                        exchange = True
+                if insert==True:
+                    while (marker < len(list1)):
+                        j = marker
+                        while (list1[j] < list1[j-1] and j>0):
+                            temp = list1[j]
+                            list1[j] = list1[j-1]
+                            list1[j-1] = temp
+                            j = j-1
+                        marker = marker+1
+            if descending==True:
+                if insert==False:
+                    if list1[j] < list1[j+1]:
+                        num=list1[j]
+                        list1[j]=list1[j+1]
+                        list1[j+1]=num
+                        exchange = True
+                if insert==True:
+                    while (marker < len(list1)):
+                        j = marker
+                        while (list1[j] > list1[j-1] and j>0):
+                            temp = list1[j]
+                            list1[j] = list1[j-1]
+                            list1[j-1] = temp
+                            j = j-1
+                    marker = marker+1
+            if dbg == True:
+                print('Debug Info')
+                print("BEFORE PASS %d: %s " %(i+1, list1)) 
+                print("%s " %list1, end="-> ")  
+                print("%s " %list1)
+                print("AFTER PASS %d: %s " %(i+1, list1))     
+            elif dbg == False:
+                ('')
+        i= i+1
+    print("Final List: ", list1)
+bubblesort(list1)
