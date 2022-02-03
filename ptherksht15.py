acd7d8282a0ae91567c3161f58f74a1d2ad92890
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2022-02-03 12:35:19 +0000

    Update ptherksht15.py

--- ptherksht15.py
+++ ptherksht15.py
@@ -7,12 +7,8 @@ L=[1, 2, 3, 4, 5, 6, 7]
 cool(L)
 
 def cool2(n):
-    list1=[]
-    if n==0:
-        return 0
+    if n<10:
+        return n
     else:
-        for i in range(n%10):
-            list1.append(n%10)
-            n=n//10
-    print(sum(list1))
-cool2(1234567)
+        return n%10 + cool2(n//10)
+print(cool2(1234567))
