c8594fc58449ad2df91ec18541c270dd9e759a33
Acc-17KBROWNE <92166999+Acc-17KBROWNE@users.noreply.github.com>
2022-03-23 20:10:52 +0000

    Update Assignment mar25.py

--- Assignment mar25.py
+++ Assignment mar25.py
@@ -1,9 +1,23 @@
 def reading():
     filename=input("Give file name: ")
-    word=0
+    word=1
     character=0
     line=0
     file1=open(filename,"r")
-    for line in file1:
-        print(file1.readline())
+    myline = file1.readline()
+    for ch in myline:
+        character+=1
+        if ch==" " or ch=="\n":
+            word+=1
+    while myline:
+        print(myline)
+        myline = file1.readline()
+        for ch in myline:
+            character+=1
+            if ch==" " or ch=="\n":
+                word+=1
+        line+=1
+    print(character, "is the amount of characters")
+    print(line, "is the amount of lines")
+    print(word, "is the amount of words")
 reading()
